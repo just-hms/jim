9b0c8d3eeada645ff45110cad6c500e39f9b50b0
just-Hms <alessandroversari1@gmail.com>
2022-11-10 12:07:00 +0100

    added - to tar options

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -6,17 +6,17 @@ echo "inserting the README.md file"
 
 echo "linux"
 env GOOS=linux;GOARCH=amd64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim ./cmd/
-cd ./out && tar vcfz jim-linux-amd64.tar.gz README.md jim && cd -
+cd ./out && tar -cvzf jim-linux-amd64.tar.gz README.md jim && cd -
 rm ./out/jim
 
 echo "darwin"
 env GOOS=darwin;GOARCH=amd64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim ./cmd/
-cd ./out && tar vcfz jim-darwin-amd64.tar.gz README.md jim && cd -
+cd ./out && tar -cvzf jim-darwin-amd64.tar.gz README.md jim && cd -
 rm ./out/jim
 
 echo "windows"
 env GOOS=windows;GOARCH=amd64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim.exe ./cmd/
-cd ./out && tar vcfz jim-windows-amd64.tar.gz README.md jim.exe && cd -
+cd ./out && tar -cvzf jim-windows-amd64.tar.gz README.md jim.exe && cd -
 rm ./out/jim.exe
 
 rm ./out/README.md
