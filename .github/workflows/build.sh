80c1a8ef34e9d540f6d1c037b73efc701ea4a7d5
just-Hms <alessandroversari1@gmail.com>
2022-11-10 10:24:53 +0100

    workflow test

---
+++ .github/workflows/build.sh
@@ -0,0 +1,24 @@
+echo "creating the ouput folder"
+mkdir -p out
+
+echo "inserting the README.md file"
+[[ -e ./README.md ]] && cp ./README.md ./out/README.md
+
+echo "linux"
+env GOOS=linux GOARCH=adm64 go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim -v ./cmd/ 
+tar -c ./out/jim-linux-adm64.tar.gz ./out/README.md ./out/jim
+rm ./out/jim
+
+echo "darwin"
+env GOOS=darwin GOARCH=adm64 go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim -v ./cmd/
+tar -c ./out/jim-darwin-adm64.tar.gz ./out/README.md ./out/jim
+rm ./out/jim
+
+echo "windows"
+env GOOS=windows GOARCH=adm64 go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim.exe -v ./cmd/
+tar -c ./out/jim-windows-adm64.tar.gz ./out/README.md ./out/jim.exe
+rm ./out/jim.exe
+
+rm ./out/README.md
+
+
