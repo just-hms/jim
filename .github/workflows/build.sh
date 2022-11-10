338daaa813103bcd9126f9f904c93f84e05c6903
just-Hms <alessandroversari1@gmail.com>
2022-11-10 11:37:27 +0100

    fix typo

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -4,19 +4,19 @@ mkdir -p out
 echo "inserting the README.md file"
 [[ -e ./README.md ]] && cp ./README.md ./out/README.md
 
-echo "linux\n"
-env GOOS=linux;GOARCH=adm64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim ./cmd/
-cd ./out && tar vcfz jim-linux-adm64.tar.gz README.md jim && cd -
+echo "linux"
+env GOOS=linux;GOARCH=amd64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim ./cmd/
+cd ./out && tar vcfz jim-linux-amd64.tar.gz README.md jim && cd -
 rm ./out/jim
 
-echo "darwin\n"
-env GOOS=darwin;GOARCH=adm64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim ./cmd/
-cd ./out && tar vcfz jim-darwin-adm64.tar.gz README.md jim && cd -
+echo "darwin"
+env GOOS=darwin;GOARCH=amd64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim ./cmd/
+cd ./out && tar vcfz jim-darwin-amd64.tar.gz README.md jim && cd -
 rm ./out/jim
 
-echo "windows\n"
-env GOOS=windows;GOARCH=adm64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim.exe ./cmd/
-cd ./out && tar vcfz jim-windows-adm64.tar.gz README.md jim.exe && cd -
+echo "windows"
+env GOOS=windows;GOARCH=amd64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim.exe ./cmd/
+cd ./out && tar vcfz jim-windows-amd64.tar.gz README.md jim.exe && cd -
 rm ./out/jim.exe
 
 rm ./out/README.md
