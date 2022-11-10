04c7c970e1dc2f431434b3ba99a9a0de84b657ff
just-Hms <alessandroversari1@gmail.com>
2022-11-10 11:19:57 +0100

    fix tar shit

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -6,17 +6,17 @@ echo "inserting the README.md file"
 
 echo "linux"
 env GOOS=linux;GOARCH=adm64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim ./cmd/ 
-tar vcfz ./out/jim-linux-adm64.tar.gz ./out/README.md ./out/jim
+cd out/ && tar vcfz jim-linux-adm64.tar.gz README.md jim && cd -
 rm ./out/jim
 
 echo "darwin"
 env GOOS=darwin;GOARCH=adm64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim ./cmd/
-tar vcfz ./out/jim-darwin-adm64.tar.gz ./out/README.md ./out/jim
+cd out/ && tar vcfz jim-darwin-adm64.tar.gz README.md jim && cd -
 rm ./out/jim
 
 echo "windows"
 env GOOS=windows;GOARCH=adm64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim.exe ./cmd/
-tar vcfz ./out/jim-windows-adm64.tar.gz ./out/README.md ./out/jim.exe
+cd out/ && tar vcfz jim-windows-adm64.tar.gz README.md jim && cd -
 rm ./out/jim.exe
 
 rm ./out/README.md
