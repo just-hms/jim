c0168dadf198b39b3602b1fa90beb6cec1302a80
just-Hms <alessandroversari1@gmail.com>
2022-11-10 11:23:37 +0100

    removed go stdout

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -5,17 +5,17 @@ echo "inserting the README.md file"
 [[ -e ./README.md ]] && cp ./README.md ./out/README.md
 
 echo "linux"
-env GOOS=linux;GOARCH=adm64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim ./cmd/ 
+env GOOS=linux;GOARCH=adm64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim ./cmd/ 1>/dev/null
 cd out/ && tar vcfz jim-linux-adm64.tar.gz README.md jim && cd -
 rm ./out/jim
 
 echo "darwin"
-env GOOS=darwin;GOARCH=adm64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim ./cmd/
+env GOOS=darwin;GOARCH=adm64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim ./cmd/ 1>/dev/null
 cd out/ && tar vcfz jim-darwin-adm64.tar.gz README.md jim && cd -
 rm ./out/jim
 
 echo "windows"
-env GOOS=windows;GOARCH=adm64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim.exe ./cmd/
+env GOOS=windows;GOARCH=adm64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim.exe ./cmd/ 1>/dev/null
 cd out/ && tar vcfz jim-windows-adm64.tar.gz README.md jim && cd -
 rm ./out/jim.exe
 
