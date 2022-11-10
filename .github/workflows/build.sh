235d6fcb27c126e4703c2476b486ebc3da913a6c
just-Hms <alessandroversari1@gmail.com>
2022-11-10 11:04:45 +0100

    hook on release

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -5,17 +5,17 @@ echo "inserting the README.md file"
 [[ -e ./README.md ]] && cp ./README.md ./out/README.md
 
 echo "linux"
-env GOOS=linux;GOARCH=adm64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim -v ./cmd/ 
+env GOOS=linux;GOARCH=adm64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim ./cmd/ 
 tar vcfz ./out/jim-linux-adm64.tar.gz ./out/README.md ./out/jim
 rm ./out/jim
 
 echo "darwin"
-env GOOS=darwin;GOARCH=adm64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim -v ./cmd/
+env GOOS=darwin;GOARCH=adm64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim ./cmd/
 tar vcfz ./out/jim-darwin-adm64.tar.gz ./out/README.md ./out/jim
 rm ./out/jim
 
 echo "windows"
-env GOOS=windows;GOARCH=adm64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim.exe -v ./cmd/
+env GOOS=windows;GOARCH=adm64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim.exe ./cmd/
 tar vcfz ./out/jim-windows-adm64.tar.gz ./out/README.md ./out/jim.exe
 rm ./out/jim.exe
 
