38119dfa726878e86f0e8c00d07c9318da6ea74f
just-Hms <alessandroversari1@gmail.com>
2022-11-12 15:38:13 +0100

    force clear added

--- actions/clear.go
+++ actions/clear.go
@@ -8,6 +8,11 @@ import (
 var Clear = &Action{
 	Value: func(args []string) {
 
+		if len(args) == 1 && args[0] == utils.ACTION_PREFIX+"force" {
+			models.Clear()
+			return
+		}
+
 		utils.Alertf("clear all commands is not reversible, are you sure? Type y or N\n")
 
 		if utils.ReadChar() != 'y' {
@@ -20,6 +25,6 @@ var Clear = &Action{
 	Description:     "clear all commands",
 	HelpDescription: " Clear all commands using this syntax\n\n     jim --clear\n\n Will remove all commands.",
 	ArgumentsCheck: func(args []string) bool {
-		return len(args) == 0
+		return len(args) == 0 || len(args) == 1
 	},
 }
