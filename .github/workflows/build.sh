ca2680efd6bf69879e82027edd97e47f9aad0ffc
just-Hms <alessandroversari1@gmail.com>
2022-11-14 02:37:46 +0100

    fix: version in CI build

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -5,17 +5,17 @@ echo "inserting the README.md file"
 [[ -e ./README.md ]] && cp ./README.md ./out/README.md
 
 echo "linux"
-env GOOS=linux;GOARCH=amd64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim ./cmd/
+env GOOS=linux;GOARCH=amd64; go build -ldflags="-X 'jim/constants.Version=$1'" -o ./out/jim ./cmd/
 cd ./out && tar -cvzf jim-linux-amd64.tar.gz README.md jim && cd -
 rm ./out/jim
 
 echo "darwin"
-env GOOS=darwin;GOARCH=amd64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim ./cmd/
+env GOOS=darwin;GOARCH=amd64; go build -ldflags="-X 'jim/constants.Version=$1'" -o ./out/jim ./cmd/
 cd ./out && tar -cvzf jim-darwin-amd64.tar.gz README.md jim && cd -
 rm ./out/jim
 
 echo "windows"
-env GOOS=windows;GOARCH=amd64; go build -ldflags="-X 'jim/utils.Version=$1'" -o ./out/jim.exe ./cmd/
+env GOOS=windows;GOARCH=amd64; go build -ldflags="-X 'jim/constants.Version=$1'" -o ./out/jim.exe ./cmd/
 cd ./out && tar -cvzf jim-windows-amd64.tar.gz README.md jim.exe && cd -
 rm ./out/jim.exe
 
